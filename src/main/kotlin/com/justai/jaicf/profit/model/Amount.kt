b77a318d8564efc1e5e4667d242918c2f19dcb7e
morfeusys <che@just-ai.com>
2020-07-24 12:01:51 +0300

    Amount recognition workaround

--- src/main/kotlin/com/justai/jaicf/profit/model/Amount.kt
+++ src/main/kotlin/com/justai/jaicf/profit/model/Amount.kt
@@ -3,4 +3,10 @@ package com.justai.jaicf.profit.model
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.content
 
-fun JsonElement.toAmount() = content.replace('o', '.').toDouble()
+fun JsonElement.toAmount() = when {
+    content == "пол" -> 0.5
+    content.startsWith("полтор") -> 1.5
+    content.startsWith("десят") -> 10.0
+    content.startsWith("дюжин") -> 12.0
+    else -> content.toDoubleOrNull()
+}
